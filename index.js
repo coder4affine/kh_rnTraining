005c959fd89eecea55b62f9f65f66060c9c1fa77
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2020-02-05 00:27:35 +0530

    commit code

--- index.js
+++ index.js
@@ -244,9 +244,7 @@ console.log(add(1, 2, 3, 3, 4, 5));
 
 // // console.log(rest);
 
-
 const arr = [...Array(1000000).keys()];
-
 
 console.time("for loop");
 
@@ -267,15 +265,281 @@ arr.forEach(element => {
 
 console.timeEnd("foreach loop");
 
+// map
 
-map
+// reduce  -> fastest /powerfull
 
-reduce  -> fastest /powerfull 
+// for in
 
-for in
+// for of
 
-for of 
+const arr = [1, 2, 3, 4, 5];
 
+const arr2 = [...arr.slice(0, 2), 6, ...arr.slice(3)];
 
+console.log(arr2);
 
+const arr1 = arr.map(item => {
+  if (item === 3) {
+    return 6;
+  }
+  return item;
+});
 
+console.log(arr1);
+
+const users = [
+  {
+    name: "yagnesh",
+    gender: "male",
+    age: 31
+  },
+  {
+    name: "advik",
+    gender: "female",
+    age: 10
+  },
+  {
+    name: "namrata",
+    gender: "female",
+    age: 28
+  },
+  {
+    name: "nirali",
+    gender: "female",
+    age: 20
+  }
+];
+
+const index = users.findIndex(x => x.name === "advik");
+const changeUsers = [
+  ...users.slice(0, index),
+  // {...users[index], gender: 'male'},
+  ...users.slice(index + 1)
+];
+
+const changeUsers1 = users.filter(x => x.name !== "advik");
+
+console.log(changeUsers1);
+
+console.log(changeUsers);
+
+const updatedUser = users.map(x => {
+  if (x.name === "advik") {
+    return { ...x, gender: "male" };
+  }
+  return x;
+});
+
+console.log(updatedUser);
+
+const arr = [1, 2, 3, 4, 5];
+
+let sum = 0;
+
+for (let index = 0; index < arr.length; index++) {
+  const element = arr[index];
+  sum += element;
+}
+
+console.log(sum);
+
+const sum1 = arr.reduce((p, c) => p + c, 0);
+
+console.log(sum1);
+
+const users = [
+  {
+    name: "yagnesh",
+    gender: "male",
+    age: 31
+  },
+  {
+    name: "advik",
+    gender: "male",
+    age: 10
+  },
+  {
+    name: "namrata",
+    gender: "female",
+    age: 28
+  },
+  {
+    name: "nirali",
+    gender: "female",
+    age: 20
+  }
+];
+
+const groupBy = users.reduce((p, c) => {
+  (p[c.gender] = p[c.gender] || []).push(c);
+  return p;
+}, {});
+
+console.log(groupBy);
+
+const a = 1;
+const b = 2;
+
+const c = a || b;
+
+const d = a && b;
+
+console.log(c);
+
+console.log(d);
+
+//   [
+//     {
+//       name: "yagnesh",
+//       gender: "male",
+//       age: 31,
+//       profession: 'cricketer'
+//     },
+//     {
+//       name: "advik",
+//       gender: "female",
+//       age: 10
+//       profession: 'cricketer'
+//     },
+//     {
+//       name: "namrata",
+//       gender: "female",
+//       age: 28
+//       profession: 'actor'
+//     },
+//     {
+//       name: "nirali",
+//       gender: "female",
+//       age: 20
+//       profession: 'actor'
+//     }
+//   ];
+
+//   const updateUser = users.reduce((p, c) => {
+//       if(c.name === 'advik') {
+//         return p;
+//       }
+//       return [...p, c]
+//   }, []);
+
+//   console.log(updateUser)
+
+const obj = { a: 1, b: 2, c: 3, d: 4 };
+
+// for(let item in obj) {
+//     console.log(item);
+//     console.log(obj[item])
+// }
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const iterator of arr) {
+  console.log(iterator);
+}
+
+for (const [key, value] of Object.entries(obj)) {
+  console.log(key);
+  console.log(value);
+}
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("resolve p1");
+  }, 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("reject p2");
+  }, 2000);
+});
+
+const func = async () => {
+  try {
+    const res = await Promise.race([p1, p2]);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+func();
+
+// p1
+// .then((val) => {
+//     console.log(val)
+//     p2
+//     .then((val1) => {
+//         console.log(val1);
+//     })
+//     .catch((err1) => {
+//         console.log(err1);
+//     })
+//  })
+// .catch((err) => { console.log(err) })
+
+// async function xyz() {
+//     await
+//     await
+
+// };
+
+function* xyz() {
+  yield 1;
+  yield 2;
+  yield 3;
+  return 1;
+}
+
+const gen = xyz();
+
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+// console.log(gen.next())
+
+for (const iterator of gen) {
+  console.log(iterator);
+}
+
+class Animal {
+  constructor(type = "animal") {
+    this.type = type;
+  }
+
+  set type(val) {
+    this._type = val.toUpperCase();
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  makeSound() {
+    setTimeout(() => {
+      console.log(this.type);
+    }, 0);
+
+    console.log("animal sound");
+  }
+}
+
+const a = new Animal();
+console.log(a.type);
+a.makeSound();
+
+// class Cat extends Animal {
+//     constructor(type = 'cat') {
+//         super(type)
+//     }
+
+//     makeSound() {
+//         super.makeSound();
+//         console.log('meow...')
+//     }
+// }
+
+// const c = new Cat()
+// console.log(c.type);
+// c.makeSound();
